7aaaeaba602d1c40a242b2c93ab3f3ab77503404
Your Name <joanclarke100@gmail.com>
2020-04-22 13:19:25 -0500

    add axios script for making network requests and OMDb API key

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const fetchData = async () => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: '9af29709',
+			s: 'avengers'
+		}
+	});
+	console.log(response.data);
+};
+
+fetchData();
