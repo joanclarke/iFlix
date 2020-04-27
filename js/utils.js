cea33bfa9e4442b75acfbdab67d0ed190861e15e
Your Name <joanclarke100@gmail.com>
2020-04-26 21:57:21 -0500

    extract utility function and place in new utils.js file

---
+++ js/utils.js
@@ -0,0 +1,13 @@
+// Debouncing an input - waiting for some time to pass
+// after the last event to actually do something
+const debounce = (func, delay = 1000) => {
+	let timeoutId;
+	return (...args) => {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+		timeoutId = setTimeout(() => {
+			func.apply(null, args);
+		}, delay);
+	};
+};
