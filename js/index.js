fafad998b40331b3aa36ba0971895e65d9d71775
Your Name <joanclarke100@gmail.com>
2020-04-28 15:06:19 -0500

    make follow up request

--- js/index.js
+++ js/index.js
@@ -53,6 +53,7 @@ const onInput = async (event) => {
 		option.addEventListener('click', () => {
 			dropdown.classList.remove('is-active');
 			input.value = movie.Title;
+			onMovieSelect(movie);
 		});
 
 		resultsWrapper.appendChild(option);
@@ -68,3 +69,13 @@ document.addEventListener('click', (event) => {
 		dropdown.classList.remove('is-active');
 	}
 });
+
+const onMovieSelect = async (movie) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: '9af29709',
+			i: movie.imdbID
+		}
+	});
+	console.log(response.data);
+};
