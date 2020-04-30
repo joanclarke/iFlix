9984eea68e3a754e01ab55c7d317e35b2cde4f5e
Your Name <joanclarke100@gmail.com>
2020-04-29 22:07:01 -0500

    refactor code: make autocomplete reusable

---
+++ js/autocomplete.js
@@ -0,0 +1,56 @@
+const createAutocomplete = ({ root }) => {
+	root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+      </div>
+    </div>
+  `;
+
+	const input = root.querySelector('input');
+	const dropdown = root.querySelector('.dropdown');
+	const resultsWrapper = root.querySelector('.results');
+
+	const onInput = async (event) => {
+		const movies = await fetchData(event.target.value);
+
+		// Close dropdown when it is empty
+		if (!movies.length) {
+			dropdown.classList.remove('is-active');
+			return;
+		}
+
+		resultsWrapper.innerHTML = '';
+		dropdown.classList.add('is-active');
+		for (let movie of movies) {
+			const option = document.createElement('a');
+			// Fix broken images
+			const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+			option.classList.add('dropdown-item');
+			option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
+      `;
+			option.addEventListener('click', () => {
+				dropdown.classList.remove('is-active');
+				input.value = movie.Title;
+				onMovieSelect(movie);
+			});
+
+			resultsWrapper.appendChild(option);
+		}
+	};
+
+	input.addEventListener('input', debounce(onInput, 500));
+
+	// Close the dropdown if root does not contain the element that was clicked on
+	document.addEventListener('click', (event) => {
+		if (!root.contains(event.target)) {
+			// To close dropdown, remove the is-active class
+			dropdown.classList.remove('is-active');
+		}
+	});
+};
--- js/index.js
+++ js/index.js
@@ -15,61 +15,6 @@ const fetchData = async (searchTerm) => {
 	return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <label><b>Search For a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (event) => {
-	const movies = await fetchData(event.target.value);
-
-	// Close dropdown when it is empty
-	if (!movies.length) {
-		dropdown.classList.remove('is-active');
-		return;
-	}
-
-	resultsWrapper.innerHTML = '';
-	dropdown.classList.add('is-active');
-	for (let movie of movies) {
-		const option = document.createElement('a');
-		// Fix broken images
-		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-		option.classList.add('dropdown-item');
-		option.innerHTML = `
-      <img src="${imgSrc}" />
-      ${movie.Title}
-		`;
-		option.addEventListener('click', () => {
-			dropdown.classList.remove('is-active');
-			input.value = movie.Title;
-			onMovieSelect(movie);
-		});
-
-		resultsWrapper.appendChild(option);
-	}
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-// Close the dropdown if root does not contain the element that was clicked on
-document.addEventListener('click', (event) => {
-	if (!root.contains(event.target)) {
-		// To close dropdown, remove the is-active class
-		dropdown.classList.remove('is-active');
-	}
-});
-
 const onMovieSelect = async (movie) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
@@ -97,24 +42,24 @@ const movieTemplate = (movieDetail) => {
 			</div>
 		</article>
 		<article class="notification is-primary">
-		<p class="title">${movieDetail.Awards}</p>
-		<p class="subtitle">Awards</p>
+			<p class="title">${movieDetail.Awards}</p>
+			<p class="subtitle">Awards</p>
 		</article>
 		<article class="notification is-primary">
-		<p class="title">${movieDetail.BoxOffice}</p>
-		<p class="subtitle">Box Office</p>
+			<p class="title">${movieDetail.BoxOffice}</p>
+			<p class="subtitle">Box Office</p>
 		</article>
 		<article class="notification is-primary">
-		<p class="title">${movieDetail.Metascore}</p>
-		<p class="subtitle">Metascore</p>
+			<p class="title">${movieDetail.Metascore}</p>
+			<p class="subtitle">Metascore</p>
 		</article>
 		<article class="notification is-primary">
-		<p class="title">${movieDetail.imdbRating}</p>
-		<p class="subtitle">IMDB Rating</p>
+			<p class="title">${movieDetail.imdbRating}</p>
+			<p class="subtitle">IMDB Rating</p>
 		</article>
 		<article class="notification is-primary">
-		<p class="title">${movieDetail.imdbVotes}</p>
-		<p class="subtitle">IMDB Votes</p>
+			<p class="title">${movieDetail.imdbVotes}</p>
+			<p class="subtitle">IMDB Votes</p>
 		</article>
 	`;
 };
