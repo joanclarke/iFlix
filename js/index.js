f4808529956d9466a285632c73a9bf64a41887e0
Your Name <joanclarke100@gmail.com>
2020-04-30 21:58:41 -0500

    Refactor code: remove all movie reference from the autocomplete.js file

--- js/index.js
+++ js/index.js
@@ -1,20 +1,3 @@
-const fetchData = async (searchTerm) => {
-	const response = await axios.get('http://www.omdbapi.com/', {
-		params: {
-			apikey: '9af29709',
-			s: searchTerm
-		}
-	});
-
-	if (response.data.Error) {
-		// return error message response
-		// console.log(response.data.Error);
-		return [];
-	}
-
-	return response.data.Search;
-};
-
 createAutoComplete({
 	root: document.querySelector('.autocomplete'),
 	renderOption(movie) {
@@ -30,6 +13,22 @@ createAutoComplete({
 	},
 	inputValue(movie) {
 		return movie.Title;
+	},
+	async fetchData(searchTerm) {
+		const response = await axios.get('http://www.omdbapi.com/', {
+			params: {
+				apikey: '9af29709',
+				s: searchTerm
+			}
+		});
+
+		if (response.data.Error) {
+			// return error message response
+			// console.log(response.data.Error);
+			return [];
+		}
+
+		return response.data.Search;
 	}
 });
 
