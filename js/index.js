f038c480db41c67c0b6ee799a9a29037ae59e43d
Your Name <joanclarke100@gmail.com>
2020-05-05 13:32:22 -0500

    bug fix: add correct color to value

--- js/index.js
+++ js/index.js
@@ -94,11 +94,11 @@ const runComparison = () => {
 		const rightSideValue = parseInt(rightStat.dataset.value);
 
 		if (rightSideValue > leftSideValue) {
-			leftStat.classList.remove('has-background-grey-light');
-			leftStat.classList.add('has-background-warning');
+			leftStat.classList.remove('has-background-warning');
+			leftStat.classList.add('has-background-grey-light');
 		} else {
-			rightStat.classList.remove('has-background-grey-light');
-			rightStat.classList.add('has-background-warning');
+			rightStat.classList.remove('has-background-warning');
+			rightStat.classList.add('has-background-grey-light');
 		}
 	});
 	// let leftSideCount;
@@ -142,23 +142,23 @@ const movieTemplate = (movieDetail) => {
 			</div>
 		</article>
 
-		<article data-value=${awards} class="notification has-text-grey-dark has-background-grey-light">
+		<article data-value=${awards} class="notification has-text-grey-dark has-background-warning">
 			<p class="title">${movieDetail.Awards}</p>
 			<p class="subtitle">Awards</p>
 		</article>
-		<article data-value=${dollars} class="notification has-text-grey-dark has-background-grey-light">
+		<article data-value=${dollars} class="notification has-text-grey-dark has-background-warning">
 			<p class="title">${movieDetail.BoxOffice}</p>
 			<p class="subtitle">Box Office</p>
 		</article>
-		<article data-value=${metascore} class="notification has-text-grey-dark has-background-grey-light">
+		<article data-value=${metascore} class="notification has-text-grey-dark has-background-warning">
 			<p class="title">${movieDetail.Metascore}</p>
 			<p class="subtitle">Metascore</p>
 		</article>
-		<article data-value=${imdbRating} class="notification has-text-grey-dark has-background-grey-light">
+		<article data-value=${imdbRating} class="notification has-text-grey-dark has-background-warning">
 			<p class="title">${movieDetail.imdbRating}</p>
 			<p class="subtitle">IMDB Rating</p>
 		</article>
-		<article data-value=${imdbVotes} class="notification has-text-grey-dark has-background-grey-light">
+		<article data-value=${imdbVotes} class="notification has-text-grey-dark has-background-warning">
 			<p class="title">${movieDetail.imdbVotes}</p>
 			<p class="subtitle">IMDB Votes</p>
 		</article>
