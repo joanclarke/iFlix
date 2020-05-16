5daed2935195dcc28519986ea24b0a91cfbb8ff6
Your Name <joanclarke100@gmail.com>
2020-05-16 18:34:19 -0500

    bug fix: fix slideshow display issue

--- js/index.js
+++ js/index.js
@@ -236,6 +236,6 @@ function showSlides(n) {
 	for (i = 0; i < dots.length; i++) {
 		dots[i].className = dots[i].className.replace(' active', '');
 	}
-	slides[slideIndex - 1].style.display = 'block';
+	slides[slideIndex - 1].style.display = 'grid';
 	dots[slideIndex - 1].className += ' active';
 }
